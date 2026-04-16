abb2f36128647b004564c5a2f02b8064320bf74a
Adel Hamed <adel.hamed.oda@gmail.com>
2026-04-16 11:50:37 +0300

    added an example for the ISerializable interface to make it easier to use

---
+++ Assets/Scripts/Test/Example for ISerializable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class TestingSaveData
+{
+    public int number = 1;
+}
+
+public class ExampleForISerializabe : MonoBehaviour, ISerializable<TestingSaveData>
+{
+    public TestingSaveData SaveData { get; set; }
+
+    private void Awake()
+    {
+        SaveData = new TestingSaveData();
+        this.Load("testing save data");
+    }
+
+    private void Update()
+    {
+        this.Save("testing save data");
+    }
+}
